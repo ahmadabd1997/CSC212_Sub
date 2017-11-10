1db7260b54325a555d099f77dc8732a9da465f0a
ahmadabd1997 <30369296+ahmadabd1997@users.noreply.github.com>
2017-11-10 17:09:12 +0300

    use the linked list
    changed the list from class to interface

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,74 +1,57 @@
-class Node<T> {
-	public T data;
-	public Node<T> next;
-	
-	public Node (T val) {
-		data = val;
-		next = null;
-	}
-}
-
 public class LinkedList<T> implements List<T> {
+
 	private Node<T> head;
 	private Node<T> current;
 	
-	public LinkedList () {
-		head = current = null;
+	public void insert(T e){ //--add element after the current
+		Node<T> tmp = new Node<T>(e);
+		if(current != null){ //if not empty
+			tmp.next = current.next;
+			current.next = tmp;
+		}
+		else head = current = tmp;//if empty
 	}
 	
-	public boolean empty () {
+	public void remove(){ //--remove the element at current. move the current to the next element or the start if removing the last element
+		if(current == head){
+			head = current = head.next;//(removed the 1st element)
+		}
+		else{
+			Node<T> tmp = head;
+			while(tmp.next != current)tmp = tmp.next;
+			tmp.next = current.next;
+			if(tmp.next != null)current = tmp.next; // current = next (removed any element in the mid)
+			else current = head; // current = start (removed last element)
+		}
+	}
+	
+	public void update(T e){ //--change the data of the current element
+		current.data = e;
+	}
+	
+	public void findFirst(){ //--move the current to the start		
+		current = head;
+	}
+	
+	public void findNext(){ //--move the current to the start		
+		current = current.next;
+	}
+	
+	public boolean full(){ //-- FALSE
+		return false;
+	}
+	
+	public boolean empty(){ //--return if the list is empty or not
 		return head == null;
+	}
+	
+	public T retrieve(){ //--return the data in the current
+		
+		return current.data;
 	}
 	
 	public boolean last () {
 		return current.next == null;
 	}
 	
-	public boolean full () {
-		return false;
-	}
-	
-	public void findFirst () {
-		current = head;
-	}
-	public void findNext () {
-		current = current.next;
-	}
-	
-	public T retrieve () {
-		return current.data;
-	}
-	
-	public void update (T val) {
-		current.data = val;
-	}
-	
-	public void insert (T val) {
-		Node<T> tmp;
-		if (head == null) {
-			current = head = new Node<T> (val);
-		} else {
-			tmp = current.next;
-			current.next = new Node<T> (val);
-			current = current.next;
-			current.next = tmp;
-		}
-	}
-	
-	public void remove () {
-		if (current == head) {
-			head = head.next;
-		} else {
-			Node<T> tmp = head;
-			while (tmp.next != current) {
-				tmp = tmp.next;
-			}
-			tmp.next = current.next;
-		}
-		if (current.next == null) {
-			current = head;
-		} else {
-			current = current.next;
-		}
-	}
 }
--- src/List.java
+++ src/List.java
@@ -1,56 +1,22 @@
 // (maybe not complete) not sure if the list need more methods or need edit!
 
-public class List<T> {
+public interface List<T> {
 	
-	private Node<T> head;
-	private Node<T> current;
-	
-	public void insert(T e){ //--add element after the current
-		Node<T> tmp = new Node<T>(e);
-		if(current != null){ //if not empty
-			tmp.next = current.next;
-			current.next = tmp;
-		}
-		else head = current = tmp;//if empty
-	}
-	
-	
-	
-	public void remove(){ //--remove the element at current. move the current to the next element or the start if removing the last element
-		if(current == head){
-			head = current = head.next;//(removed the 1st element)
-		}
-		else{
-			Node<T> tmp = head;
-			while(tmp.next != current)tmp = tmp.next;
-			tmp.next = current.next;
-			if(tmp.next != null)current = tmp.next; // current = next (removed any element in the mid)
-			else current = head; // current = start (removed last element)
-		}
-	}
-	
-	public void update(T e){ //--change the data of the current element
-		current.data = e;
-	}
-	
-	public void findFirst(){ //--move the current to the start		
-		current = head;
-	}
-	
-	public void findNext(){ //--move the current to the start		
-		current = current.next;
-	}
-	
-	public boolean full(){ //-- FALSE
-		return false;
-	}
-	
-	public boolean empty(){ //--return if the list is empty or not
-		return head == null;
-	}
-	
-	public T retrieve(){ //--return the data in the current
+	public void insert(T e);
 		
-		return current.data;
-	}
+	public void remove();
+	
+	public void update(T e);
+	
+	public void findFirst();
+	
+	public void findNext();
+	
+	public boolean full();
+	
+	public boolean empty();
+	
+	public T retrieve();
+	
+	public boolean last ();
 }
